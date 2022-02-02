1fcb7c8e2b1a648c904a3800f7d7e3fc3d503539
1620585 <1620585@mobis.co.kr>
2022-02-02 14:35:04 -0800

    Day 5 loop

--- myfile.cpp
+++ myfile.cpp
@@ -1,56 +1,45 @@
+#include <bits/stdc++.h>
 using namespace std;
-#include <iostream>
 
-class Person{
-    public:
-        int age;
-        Person(int initialAge);
-        void amIOld();
-        void yearPasses();
-    };
+string ltrim(const string &);
+string rtrim(const string &);
 
-    Person::Person(int initialAge){
-	    if(initialAge >0)
-	        age = initialAge;// Add some more code to run some checks on initialAre
-	    else 
-	    {
-		    cout<<"Age is not valid, setting age to 0."<<endl;
-		    age =0;
-	    }
 
-    }
 
-    void Person::amIOld(){
-        // Do some computations in here and print out the correct statement to the console 
-	if(age<13)
-		cout<<"You are young."<<endl;
-	else if(age <18)
-		cout <<"You are a teenager."<<endl;
-	else 
-		cout << "You are old."<<endl;
-    }
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
 
-    void Person::yearPasses(){
-	    age++;
-        // Increment the age of the person in here
+    int n = stoi(ltrim(rtrim(n_temp)));
 
-    }
+	for( int i=1;i<11;i++)
+	{
+		cout << n <<" x " << i <<" = " << n*i <<endl;
+	}
 
-int main(){
-    int t;
-	int age;
-    cin >> t;
-    for(int i=0; i < t; i++) {
-    	cin >> age;
-        Person p(age);
-        p.amIOld();
-        for(int j=0; j < 3; j++) {
-        	p.yearPasses(); 
-        }
-        p.amIOld();
-      
-		cout << '\n';
-    }
 
     return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
 }
