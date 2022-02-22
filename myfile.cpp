b8db41230b075fefbfa989b2b2529556437be36e
1620585 <1620585@mobis.co.kr>
2022-02-22 09:59:45 -0800

    day9

--- myfile.cpp
+++ myfile.cpp
@@ -1,55 +1,63 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-#include <map>
+#include <bits/stdc++.h>
+
 using namespace std;
 
 string ltrim(const string &);
+string rtrim(const string &);
 
-int main() {
-	/* Enter your code here. Read input from STDIN. Print output to STDOUT */   
-	int n;
-	string tmp_input;
-	getline(cin,tmp_input);
-	n = stoi(tmp_input);
-	map <string,string> phonebook;
-	vector<string> quary(n);
-	string tmp_name, tmp_phonenumber;
-	for(int i=0;i<n;i++){
-		cin>>tmp_name;
-		getline(cin,tmp_phonenumber);
-		tmp_phonenumber = ltrim(tmp_phonenumber);
-//		cin>>tmp_phonenumber;
-		if(tmp_phonenumber.length() !=0)
-			phonebook[tmp_name]=tmp_phonenumber;
-	}
-//	getline(cin,tmp_name);
-	int qu_num=0;
-	for(;qu_num<n;qu_num++){
-		getline(cin,quary[qu_num]);
-		if(quary[qu_num].length()==0) break;
-	}	
-	for(int i=0;i<qu_num;i++){
-		if(phonebook.count(quary[i]))
-				cout << quary[i]<<"="<<phonebook[quary[i]]<<endl;
-		else 
-			cout<<"Not found"<<endl;
-	}
+/*
+ *  * Complete the 'factorial' function below.
+ *   *
+ *    * The function is expected to return an INTEGER.
+ *     * The function accepts INTEGER n as parameter.
+ *      */
+
+int factorial(int n) {
+	if(n<=1) 
+		return 1;
+	else 
+		return n * factorial(n-1);
+}
+
+int main()
+{
+	cout <<"hi"<<endl;
+	cout << getenv("PATH")<<endl;
+
+	ofstream fout("./hi.txt");
+	string n_temp;
+	getline(cin, n_temp);
+
+	int n = stoi(ltrim(rtrim(n_temp)));
+
+	int result = factorial(n);
+
+	fout << result << "\n";
+
+	fout.close();
 
 	return 0;
 }
 
+string ltrim(const string &str) {
+	string s(str);
 
-   string ltrim(const string &str) {
-	       string s(str);
-
-	           s.erase(
-		   s.begin(),
-		   find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+	s.erase(
+			s.begin(),
+			find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
 	       );
 
-		       return s;
-   }
+	return s;
+}
+
+string rtrim(const string &str) {
+	string s(str);
+
+	s.erase(
+			find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+			s.end()
+	       );
+
+	return s;
+}
 
