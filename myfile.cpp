c947b60e24ed184bbba12552d3b25e9b30735a65
1620585 <1620585@mobis.co.kr>
2022-02-23 11:41:01 -0800

    day 13

--- myfile.cpp
+++ myfile.cpp
@@ -1,87 +1,87 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-string ltrim(const string &);
-string rtrim(const string &);
-vector<string> split(const string &);
 
-
-
-int main()
-{
-
-	vector<vector<int>> arr(6);
-
-	for (int i = 0; i < 6; i++) {
-		arr[i].resize(6);
-
-		string arr_row_temp_temp;
-		getline(cin, arr_row_temp_temp);
-
-		vector<string> arr_row_temp = split(rtrim(arr_row_temp_temp));
-
-		for (int j = 0; j < 6; j++) {
-			int arr_row_item = stoi(arr_row_temp[j]);
-
-			arr[i][j] = arr_row_item;
+class Person{
+	protected:
+		string firstName;
+		string lastName;
+		int id;
+	public:
+		Person(string firstName, string lastName, int identification){
+			this->firstName = firstName;
+			this->lastName = lastName;
+			this->id = identification;
 		}
-	}
+		void printPerson(){
+			cout<< "Name: "<< lastName << ", "<< firstName <<"\nID: "<< id << "\n"; 
+		}
 
-	int max_num=-45;
-	for (int i=0; i<4; i++)
-	for (int j=0; j<4;j++){
-		int a,b,c,d,e,f,g,tmp_num=0;
-		a = arr[i][j];
-		b = arr[i][j+1];
-		c = arr[i][j+2];
-		d = arr[i+1][j+1];
-		e = arr[i+2][j];
-		f =  arr[i+2][j+1];
-		g = arr[i+2][j+2];
-		tmp_num = a+b+c+d+e+f+g;
-		if (max_num < tmp_num)
-			max_num = tmp_num;
+};
+
+
+
+class Student :  public Person{
+	private:
+		vector<int> testScores;  
+	public:
+		Student(string firstName, string lastName, int identification, vector<int> scores):Person(firstName, lastName, identification),testScores(scores){}
+		char calculate()
+		{
+			int total_score=0;
+			double avg=0;
+			for(int i=0;i<testScores.size();i++)
+				total_score += testScores.at(i);
+			avg = 1.0*total_score/testScores.size();
+
+			if(avg<=100 and avg>=90)
+				return 'O';
+			else if(avg >=80)
+				return 'E';
+			else if(avg>=70)
+				return 'A';
+			else if(avg>=55)
+				return 'P';
+			else if(avg>=40)
+				return 'D';
+			else return 'T';
+
+		}	
+
+		/*	
+		 *   Class Constructor
+		 *   
+		 *   Parameters:
+		 *   firstName - A string denoting the Person's first name.
+		 *   lastName - A string denoting the Person's last name.
+		 *   id - An integer denoting the Person's ID number.
+		 *   scores - An array of integers denoting the Person's test scores.
+		 */
+		// Write your constructor here
+
+		/*	
+		 *   Function Name: calculate
+		 *   Return: A character denoting the grade.
+		 */
+		// Write your function here
+};
+
+int main() {
+	string firstName;
+	string lastName;
+	int id;
+	int numScores;
+	cin >> firstName >> lastName >> id >> numScores;
+	vector<int> scores;
+	for(int i = 0; i < numScores; i++){
+		int tmpScore;
+		cin >> tmpScore;
+		scores.push_back(tmpScore);
 	}
-	cout << max_num << endl;
+	Student* s = new Student(firstName, lastName, id, scores);
+	s->printPerson();
+	cout << "Grade: " << s->calculate() << "\n";
 	return 0;
-}
-
-string ltrim(const string &str) {
-	string s(str);
-
-	s.erase(
-			s.begin(),
-			find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-	       );
-
-	return s;
-}
-
-string rtrim(const string &str) {
-	string s(str);
-
-	s.erase(
-			find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-			s.end()
-	       );
-
-	return s;
-}
-
-vector<string> split(const string &str) {
-	vector<string> tokens;
-
-	string::size_type start = 0;
-	string::size_type end = 0;
-
-	while ((end = str.find(" ", start)) != string::npos) {
-		tokens.push_back(str.substr(start, end - start));
-
-		start = end + 1;
-	}
-
-	tokens.push_back(str.substr(start));
-
-	return tokens;
 }
