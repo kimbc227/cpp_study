c09302664bd0f6833aeaa8ab3de126f9177c76b7
1620585 <1620585@mobis.co.kr>
2022-02-23 13:51:01 -0800

    day 13

--- myfile.cpp
+++ myfile.cpp
@@ -1,87 +1,64 @@
-#include <iostream>
+#include <cmath>
+#include <cstdio>
 #include <vector>
-
+#include <iostream>
+#include <algorithm>
+#include <string>
 using namespace std;
-
-
-class Person{
+class Book{
 	protected:
-		string firstName;
-		string lastName;
-		int id;
+		string title;
+		string author;
 	public:
-		Person(string firstName, string lastName, int identification){
-			this->firstName = firstName;
-			this->lastName = lastName;
-			this->id = identification;
+		Book(string t,string a){
+			title=t;
+			author=a;
 		}
-		void printPerson(){
-			cout<< "Name: "<< lastName << ", "<< firstName <<"\nID: "<< id << "\n"; 
-		}
+		virtual void display()=0;
 
 };
 
+// Write your MyBook class here
+class MyBook:public Book{
+	private :
+		int price;
+
+	public : 
+	MyBook(string t, string a, int p):Book(t,a), price(p){}
+	
+	void display(){
+		cout << "Title: "<< title<<endl;
+		cout << "Author: "<<author<<endl;
+		cout << "Price: "<<price<<endl;
+	}
 
 
-class Student :  public Person{
-	private:
-		vector<int> testScores;  
-	public:
-		Student(string firstName, string lastName, int identification, vector<int> scores):Person(firstName, lastName, identification),testScores(scores){}
-		char calculate()
-		{
-			int total_score=0;
-			double avg=0;
-			for(int i=0;i<testScores.size();i++)
-				total_score += testScores.at(i);
-			avg = 1.0*total_score/testScores.size();
-
-			if(avg<=100 and avg>=90)
-				return 'O';
-			else if(avg >=80)
-				return 'E';
-			else if(avg>=70)
-				return 'A';
-			else if(avg>=55)
-				return 'P';
-			else if(avg>=40)
-				return 'D';
-			else return 'T';
-
-		}	
-
-		/*	
-		 *   Class Constructor
-		 *   
-		 *   Parameters:
-		 *   firstName - A string denoting the Person's first name.
-		 *   lastName - A string denoting the Person's last name.
-		 *   id - An integer denoting the Person's ID number.
-		 *   scores - An array of integers denoting the Person's test scores.
-		 */
-		// Write your constructor here
-
-		/*	
-		 *   Function Name: calculate
-		 *   Return: A character denoting the grade.
-		 */
-		// Write your function here
 };
+//   Class Constructor
+//   
+//   Parameters:
+//   title - The book's title.
+//   author - The book's author.
+//   price - The book's price.
+//
+// Write your constructor here
+
+
+//   Function Name: display
+//   Print the title, author, and price in the specified format.
+//
+// Write your method here
+
+// End class
 
 int main() {
-	string firstName;
-	string lastName;
-	int id;
-	int numScores;
-	cin >> firstName >> lastName >> id >> numScores;
-	vector<int> scores;
-	for(int i = 0; i < numScores; i++){
-		int tmpScore;
-		cin >> tmpScore;
-		scores.push_back(tmpScore);
-	}
-	Student* s = new Student(firstName, lastName, id, scores);
-	s->printPerson();
-	cout << "Grade: " << s->calculate() << "\n";
+	string title,author;
+	int price;
+	getline(cin,title);
+	getline(cin,author);
+	cin>>price;
+	MyBook novel(title,author,price);
+	novel.display();
 	return 0;
 }
+
