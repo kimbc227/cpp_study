2176f72e358eba4bbefa5940f94075c038b46d03
1620585 <1620585@mobis.co.kr>
2022-02-24 10:19:15 -0800

    day15

--- myfile.cpp
+++ myfile.cpp
@@ -1,62 +1,57 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
-
-using namespace std;
-
-class Difference {
-	private:
-		vector<int> elements;
-
+#include <cstddef>
+using namespace std;	
+class Node
+{
 	public:
-		int maximumDifference;
+		int data;
+		Node *next;
+		Node(int d){
+			data=d;
+			next=NULL;
+		}
+};
+class Solution{
+	public:
 
-		// Add your code here
-	Difference(vector<int> a):elements(a), maximumDifference(0){}
-	void computeDifference()
-	{			
-		for(int i=0;i<elements.size()-1;i++)
+		Node* insert(Node *head,int data)
 		{
-			int tmp_element = elements.at(i);
-
-			for(int j=i+1;j<elements.size();j++)
-			{	
-				int tmp_2nd_element = 0, tmp_difference=0;
-				tmp_2nd_element = elements.at(j);
-				tmp_difference = tmp_element - tmp_2nd_element;
-				if(tmp_difference<0)
-					tmp_difference=tmp_difference * -1;
-				if( tmp_difference> maximumDifference)
-					maximumDifference = tmp_difference;
-
+			//Complete this method
+			//Node new_Node(2);
+			//Node *new_Node = malloc(sizeof(Node));
+			if(head == NULL)
+			{
+				head = new Node(data);
 			}
+			else{
+				head->next = insert(head->next,data);
+			}
+			
+				return head;
+
+
 		}
 
-	}
+		void display(Node *head)
+		{
+			Node *start=head;
+			while(start)
+			{
+				cout<<start->data<<" ";
+				start=start->next;
+			}
+		}
+};
+int main()
+{
+	Node* head=NULL;
+	Solution mylist;
+	int T,data;
+	cin>>T;
+	while(T-->0){
+		cin>>data;
+		head=mylist.insert(head,data);
+	}	
+	mylist.display(head);
 
-	
-}; // End of Difference class
-
-int main() {
-	int N;
-	cin >> N;
-
-	vector<int> a;
-
-	for (int i = 0; i < N; i++) {
-		int e;
-		cin >> e;
-
-		a.push_back(e);
-	}
-
-	Difference d(a);
-
-	d.computeDifference();
-
-	cout << d.maximumDifference;
-
-	return 0;
 }
