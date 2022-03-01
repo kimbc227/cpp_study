97c720b821ece1b3241dfa28ea942f6cbccb5d71
1620585 <1620585@mobis.co.kr>
2022-02-28 16:14:34 -0800

    day 17

--- myfile.cpp
+++ myfile.cpp
@@ -1,57 +1,22 @@
-#include <iostream>
-#include <cstddef>
-using namespace std;	
-class Node
-{
-	public:
-		int data;
-		Node *next;
-		Node(int d){
-			data=d;
-			next=NULL;
-		}
-};
-class Solution{
-	public:
+#include <bits/stdc++.h>
 
-		Node* insert(Node *head,int data)
-		{
-			//Complete this method
-			//Node new_Node(2);
-			//Node *new_Node = malloc(sizeof(Node));
-			if(head == NULL)
-			{
-				head = new Node(data);
-			}
-			else{
-				head->next = insert(head->next,data);
-			}
-			
-				return head;
+using namespace std;
 
 
-		}
 
-		void display(Node *head)
-		{
-			Node *start=head;
-			while(start)
-			{
-				cout<<start->data<<" ";
-				start=start->next;
-			}
-		}
-};
 int main()
 {
-	Node* head=NULL;
-	Solution mylist;
-	int T,data;
-	cin>>T;
-	while(T-->0){
-		cin>>data;
-		head=mylist.insert(head,data);
-	}	
-	mylist.display(head);
+	string S;
+	getline(cin, S);
 
+	int result =0;
+	try{
+		result = stoi(S);
+		cout << result <<endl;
+	}
+	catch(exception& error){
+		cout<<"Bad String"<<endl;
+	}
+	return 0;
 }
+
