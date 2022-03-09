e7b1badd3bed72701b042f787833e8877671214a
1620585 <1620585@mobis.co.kr>
2022-03-08 16:56:48 -0800

    day20

--- myfile.cpp
+++ myfile.cpp
@@ -1,56 +1,75 @@
 #include <iostream>
+#include <cstddef>
 
+using namespace std;	
 
-using namespace std;
+class Node{
+	public:
+		int data;
+		Node *left;
+		Node *right;
+		Node(int d){
+			data = d;
+			left = NULL;
+			right = NULL;
+		}
+};
+class Solution{
+	public:
+		Node* insert(Node* root, int data) {
+			if(root == NULL) {
+				return new Node(data);
+			}
+			else {
+				Node* cur;
+				if(data <= root->data){
+					cur = insert(root->left, data);
+					root->left = cur;
+				}
+				else{
+					cur = insert(root->right, data);
+					root->right = cur;
+				}
 
+				return root;
+			}
+		}
 
+		int getHeight(Node* root){
+			//Write your code here
+			int left_length=0, right_length=0;
+			if(root->left ==NULL && root->right==NULL)
+				return 0;
+			if(root-> right != NULL)
+			{
+			//	Solution left_recursive;
+				right_length = this->getHeight(root->right)+1;
+				
+			}			
+			if(root->left != NULL)
+				left_length = this->getHeight(root->left)+1;
+			return (right_length>left_length)?right_length:left_length;	
+		}
 
+}; //End of Solution
 
+int main() {
+	Solution myTree;
+	Node* root = NULL;
+	int t;
+	int data;
 
-int* func1(){
+	cin >> t;
 
-	int array2[3] = {1,2,3};
-	
-	return array2;
+	while(t-- > 0){
+		cin >> data;
+		root = myTree.insert(root, data);
+	}
+	int height = myTree.getHeight(root);
+	cout << height;
+
+	return 0;
 }
-
-
-void func2(int * a){
-//	a = new int[3];
-
-
-	a[0] =4;
-	a[1] =3;
-	a[2] =5;
-	//a = a+3;
-	a++;a++;a++;
-	a = new int[2];
-
-	a[0]=4;
-	a[1] = 0;
-
-
-	
-
-}
-
-int main()
-{
-	int array1[]= {2,3,4};
-	
-//	int *array1;
-
-
-	func2(array1);
-
-	cout << array1[0] << array1[1] << array1[2]<<array1[3]<<array1[4] << endl;
-
-
-	
-
-}
-
-
 
 
 
