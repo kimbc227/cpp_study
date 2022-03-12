f8af166b69150fd3b5d998e59c00ec3104d2fae8
1620585 <1620585@mobis.co.kr>
2022-03-11 16:24:44 -0800

    myfile.cpp

--- myfile.cpp
+++ myfile.cpp
@@ -1,96 +1,54 @@
-#include <cstddef>
 #include <cmath>
 #include <cstdio>
 #include <vector>
 #include <iostream>
 #include <algorithm>
-using namespace std;	
-class Node
-{
-	public:
-		int data;
-		Node *next;
-		Node(int d){
-			data=d;
-			next=NULL;
-		}
-};
-class Solution{
-	public:
+using namespace std;
 
-		Node* removeDuplicates(Node *head)
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+	int N;
+
+	cin >> N;
+	vector <int> input(N);
+
+	for (int i =0; i<N; i++)
+	{
+		cin>>input[i];
+	}
+
+
+
+	for(int i=0;i<N;i++)
+	{
+		int isprime=1;
+
+		if(input[i] == 1)
+			isprime=0;	
+
+		for(int j = 2;j*j<=input[i];j++)
 		{
-			//Write your code here
-			Node * cur=head;
-			do{
-
-
-
-				for(Node * compare=cur;compare->next !=NULL;)
-				{	
-					Node * prev= compare;	
-					compare = compare->next;
-					if(cur->data == compare->data)
-					{
-						prev->next = compare->next;
-						compare = prev;	
-					}
-
-				}
-								
-				cur = cur->next;
-			}while(cur !=NULL);
-
-
-			return head;
-		}
-
-		Node* insert(Node *head,int data)
-		{
-			Node* p=new Node(data);
-			if(head==NULL){
-				head=p;  
-
-			}
-			else if(head->next==NULL){
-				head->next=p;
-
-			}
-			else{
-				Node *start=head;
-				while(start->next!=NULL){
-					start=start->next;
-				}
-				start->next=p;   
-
-			}
-			return head;
-
-
-		}
-		void display(Node *head)
-		{
-			Node *start=head;
-			while(start)
+			if(input[i] %j ==0)
 			{
-				cout<<start->data<<" ";
-				start=start->next;
+				isprime = 0;
+				break;
 			}
+
 		}
-};
+	
 
-int main()
-{
-	Node* head=NULL;
-	Solution mylist;
-	int T,data;
-	cin>>T;
-	while(T-->0){
-		cin>>data;
-		head=mylist.insert(head,data);
-	}	
-	head=mylist.removeDuplicates(head);
+		if(isprime ==0)
+			cout << "Not prime"<<endl;
 
-	mylist.display(head);
+		else 
+			cout << "Prime" <<endl;		
+
+
+
+	}
+
+	return 0;
+
 
 }
