aa0ccbbb6d875061d1fd396b1afe8b7fbe1dbde4
1620585 <1620585@mobis.co.kr>
2022-03-14 11:55:14 -0700

    day26

--- myfile.cpp
+++ myfile.cpp
@@ -7,48 +7,39 @@ using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
-	int N;
+	/* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+		
+	int returned_day, returned_month, returned_year;
+	int expected_day, expected_month, expected_year;
+	int fine = 0;
 
-	cin >> N;
-	vector <int> input(N);
+	cin >> returned_day >> returned_month >> returned_year;
+	cin >> expected_day>> expected_month >> expected_year;
 
-	for (int i =0; i<N; i++)
-	{
-		cin>>input[i];
-	}
+	cout << "return day : " << returned_day<<endl;
+	cout << "mretur nonth : " << returned_month<<endl;
+	cout << "return  year : " << returned_year<<endl;
+	cout << "expected day : " << expected_day<<endl;
+	cout << "expected nonth : " << expected_month<<endl;
+	cout << "expected  year : " << expected_year<<endl;
 
+	if((returned_year<expected_year) || 
+	((returned_year == expected_year)&&(returned_month<expected_month)))
+//	((returned_year == expectec_year)&&(returned_month==expected_month)&& ( expected_day>returned_day) ) 	
+		fine = 0; 
+	else 
 
+	if (expected_year<returned_year)
+		fine = 10000;
 
-	for(int i=0;i<N;i++)
-	{
-		int isprime=1;
+	else if(expected_month<returned_month)
+		fine = 500 *(returned_month - expected_month);
+	else if(expected_day<returned_day) 
+		fine = 15 * ( returned_day-expected_day);
 
-		if(input[i] == 1)
-			isprime=0;	
+	cout << fine<<endl;
+		
 
-		for(int j = 2;j*j<=input[i];j++)
-		{
-			if(input[i] %j ==0)
-			{
-				isprime = 0;
-				break;
-			}
-
-		}
-	
-
-		if(isprime ==0)
-			cout << "Not prime"<<endl;
-
-		else 
-			cout << "Prime" <<endl;		
-
-
-
-	}
 
 	return 0;
-
-
 }
