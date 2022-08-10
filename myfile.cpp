968085ff535e743a2404318e86276b4419ee9ec3
1620585 <1620585@mobis.co.kr>
2022-08-10 16:22:45 -0700

    r value l value

--- myfile.cpp
+++ myfile.cpp
@@ -1,116 +1,33 @@
-#include <bits/stdc++.h>
+#include<iostream>
 
 using namespace std;
 
-string ltrim(const string &);
-string rtrim(const string &);
-vector<string> split(const string &);
 
-/*
- * Complete the 'bitwiseAnd' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts following parameters:
- *  1. INTEGER N
- *  2. INTEGER K
- */
+class Integer
+{
+	public:
+		Integer() : mData(0){cout<< "1st case" << endl;}
+		Integer(const Integer& data) : mData(data.mData) { cout << " 2nd case" << endl;}
+		Integer(const int data) : mData(data) { cout << "3rd case" <<endl;}
 
-int bitwiseAnd(int N, int K) {
-	int result =0;
-	for(int i=0;i<=N-1;i++)
-		for(int j=i+1;j<=N;j++)
-		{
-			int tmp;
-			tmp = i&j;
-			if(tmp < K)
-				if(result<tmp)
-					result =tmp;
-		}
+		void printdata()
+		{ cout << "hi : " << mData << endl;}
+		
+		void printdata_test(Integer data)
+		{ data.printdata();}
+	private:
+		int mData;
 
-	return result;
-
-}
-
-#define mytest
+};
 
 
 int main()
 {
-	ofstream fout("./my_test_result.txt");
 
-	string t_temp;
-	getline(cin, t_temp);
+	Integer myInt(3);
+	Integer testInt(myInt);
 
-	int t = stoi(ltrim(rtrim(t_temp)));
+	testInt.printdata();
+
 	
-//	vector<int> result;
-
-	for (int t_itr = 0; t_itr < t; t_itr++) {
-		string first_multiple_input_temp;
-		getline(cin, first_multiple_input_temp);
-
-		vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
-
-		int count = stoi(first_multiple_input[0]);
-
-		int lim = stoi(first_multiple_input[1]);
-
-		int res = bitwiseAnd(count, lim);
-		
-		fout << res << "\n";
-
-		//result.push_back(res); 
-#ifdef mytest
-		cout<< res << endl;
-
-#endif
-	}
-
-/*
-	for(vector<int>::iterator i=result.begin();i!=result.end();i++)
-		cout<<*i<<endl;
-*/
-	fout.close();
-
-	return 0;
 }
-
-string ltrim(const string &str) {
-	string s(str);
-
-	s.erase(
-			s.begin(),
-			find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-	       );
-
-	return s;
-}
-
-string rtrim(const string &str) {
-	string s(str);
-
-	s.erase(
-			find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-			s.end()
-	       );
-
-	return s;
-}
-
-vector<string> split(const string &str) {
-	vector<string> tokens;
-
-	string::size_type start = 0;
-	string::size_type end = 0;
-
-	while ((end = str.find(" ", start)) != string::npos) {
-		tokens.push_back(str.substr(start, end - start));
-
-		start = end + 1;
-	}
-
-	tokens.push_back(str.substr(start));
-
-	return tokens;
-}
-
