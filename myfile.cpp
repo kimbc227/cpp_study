717918d4c6066dec9c00c3318f17b0665100113a
1620585 <1620585@mobis.co.kr>
2022-09-08 14:43:19 -0700

    day 6

--- myfile.cpp
+++ myfile.cpp
@@ -1,22 +1,73 @@
 #include <iostream>
 
-
 using namespace std;
 
-int& func1(int& a) { return a; } 
-int func2(int b) { return b; }
+class Integer
+{
+public:
+	Integer() : mData(nullptr) 
+	{ 
+		mData = new int(0);
+		cout << "디폴트 생성자 호출!" << endl; 
+	}
+	Integer(const int data)
+	{
+		mData = new int(data);
+		cout << "const int data 일반 생성자 호출!" << endl;
+	}
+	Integer(const Integer& data) 
+	{ 
+		mData = new int(data.getData());
+		cout << "const Integer& data 복사 생성자 호출!" << endl;
+	}
+	Integer(Integer&& data) : mData(data.mData)
+	{
+		cout << "const Integer&& data 이동 생성자 호출! " << data.mData << endl;
+
+		data.mData = nullptr;
+		
+	}
+	virtual ~Integer() 
+	{ 
+		if (mData)
+		{
+			cout << "소멸자 호출! " << mData << endl;
+			delete mData;
+			mData = nullptr;
+		}
+		else
+			cout << "소멸자 호출! " << mData << endl;
+	}
+
+	
+	int getData() const { return *mData; }
+	void setData(const int data) { *mData = data; }
+	void setNull() { mData = nullptr; }
+
+	Integer operator+(Integer& data)
+	{
+		Integer ret;
+		cout << "연산자 오버로딩(+) 함수 호출! " << ret.mData << endl;
+		ret.setData(this->getData() + data.getData());
+		return ret;
+	}
+	void Print()
+	{
+		cout << *mData << endl;
+	}
+private:
+	int *mData;
+};
+
 
 int main()
 {
-	int a = 3;
-	func1(a) = 4;
-	cout << &func1(a) << endl;
-	cout << a << endl;
-	int b = 2;
-	a = func2(b); // 가능 
-//	func2(b) = 5; // 오류1 
-//	cout << &func2(b) << endl; // 오류 2 }
-
-
-
+	Integer x(5);
+	Integer y(10);
+	cout << "--------------" << endl;
+	Integer z = x+ y;
+	x.Print();
+	y.Print();
+	z.Print();
+	cout << "--------------" << endl;
 }
